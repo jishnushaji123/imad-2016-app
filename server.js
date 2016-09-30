5bfcc12be33be021c46849d41ee8b99d3a1d48b0
JISHNNU SHAJI <jishnushajitdr6@gmail.com>
2016-09-30 21:15:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('/ethixx', function (req, res)
 {
     res.sendFile(path.join(__dirname, 'ui', 'home.html'));
 });
+app.get('/ethixx', function (req, res)
+{
+    res.sendFile(path.join(__dirname, 'ui', 'index1.html'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
